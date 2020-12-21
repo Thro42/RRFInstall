f0b36575a57e1cab65c8a1416c36651b2ba767cd
Thro42 <thro@thomitzek.com>
2020-12-21 14:29:52 +0100

    Update CoppySD.sh

--- CoppySD.sh
+++ CoppySD.sh
@@ -1,4 +1,9 @@
-cp -r /home/pi/RRFInstall/sys/ /opt/dsf/sd/sys/
-cp -r /home/pi/RRFInstall/macros/ /opt/dsf/sd//macros/
-cp -r /home/pi/RRFInstall/filaments/ /opt/dsf/sd/filaments/
-cp -r /home/pi/RRFInstall/gcodes/ /opt/dsf/sd/gcodes/
+echo "--- Copy sys files ---"
+sudo cp -r /home/pi/RRFInstall/sys/ /opt/dsf/sd/sys/
+echo "--- Copy Macros files ---"
+sudo cp -r /home/pi/RRFInstall/macros/ /opt/dsf/sd//macros/
+echo "--- Copy Filamets files ---"
+sudo cp -r /home/pi/RRFInstall/filaments/ /opt/dsf/sd/filaments/
+echo "--- Copy GCodes files ---"
+sudo cp -r /home/pi/RRFInstall/gcodes/ /opt/dsf/sd/gcodes/
+echo "--- done ---"
