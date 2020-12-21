14b23ae2de356afc993f980cc7753fcc39b5a415
Thro42 <thro@thomitzek.com>
2020-12-21 22:22:27 +0100

    Update CoppySD.sh
    Add chmod

--- CoppySD.sh
+++ CoppySD.sh
@@ -1,9 +1,15 @@
 echo "--- Copy sys files ---"
 sudo cp -r /home/pi/RRFInstall/sys/ /opt/dsf/sd/
+sudo chmod 666 /opt/dsf/sd/sys/*.g
+sudo chmod 666 /opt/dsf/sd/sys/*.csv
 echo "--- Copy Macros files ---"
 sudo cp -r /home/pi/RRFInstall/macros/ /opt/dsf/sd/
+sudo chmod 666 /opt/dsf/sd/macros/*.*
 echo "--- Copy Filamets files ---"
 sudo cp -r /home/pi/RRFInstall/filaments/ /opt/dsf/sd/
+sudo chmod 666 /opt/dsf/sd/filaments/*.*
+sudo chmod 666 /opt/dsf/sd/filaments/PLA/*.*
 echo "--- Copy GCodes files ---"
 sudo cp -r /home/pi/RRFInstall/gcodes/ /opt/dsf/sd/
+sudo chmod 666 /opt/dsf/sd/gcodes/*.*
 echo "--- done ---"
